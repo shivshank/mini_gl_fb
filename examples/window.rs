c9e323c2480edb2208e03febccadee926c673a24
shivshank <shivshank@users.noreply.github.com>
2018-05-29 23:33:20 -0400

    Fix example typo

--- examples/window.rs
+++ examples/window.rs
@@ -4,5 +4,5 @@ fn main() {
     let mut fb = mini_gl_fb::gotta_go_fast("Hello world!", 800, 600);
     let buffer = vec![[128u8, 0, 0, 255]; 800 * 600];
     fb.update_buffer(&buffer);
-    fb.persist(true);
+    fb.persist();
 }
